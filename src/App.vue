85813e26749b0c641fbbce9d82a4b1ee2ffceb1d
Rodrigo Lopez <rodrigo.lapidus@gmail.com>
2022-09-02 17:27:17 +0200

    Weather API setup

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { createDOMCompilerError } from '@vue/compiler-dom';
+import HelloWorld from './components/HelloWorld.vue';
+  
+const APIkey = "fdd16dabda152dda4648159c6ca7973f";
+
+const city = "Detroid";
+
+created() {
+  this.getCurrentWeather();
+}
+
+getCurrentWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.APIkey}`)
+    
+    // check if the data is showing
+    .then((res) =>
+      console.log(res.data);
+    )};
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Quicksand', sans-serif;
+  }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import AddCity from "../views/AddCity.vue";
+
+Vue.useA(VueRouter);
+
+const routes = [
+    {
+        path: "/",
+        name: "AddCity",
+        component: AddCity,
+    },
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
